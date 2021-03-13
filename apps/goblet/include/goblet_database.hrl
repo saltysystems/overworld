117c036429542a475420112850631d040162c8e0
Lincoln Bryant <cmdrk@protonmail.com>
2021-03-12 23:22:24 -0600

    added a match record, changed the player record a bit

--- apps/goblet/include/goblet_database.hrl
+++ apps/goblet/include/goblet_database.hrl
@@ -5,8 +5,11 @@
 
 % Records representing the database tables in Mnesia
 -record(goblet_account,{email, id, hash, salt, first_login, last_login, player_ids}).
--record(goblet_player,{name, id, title, appearance, role, stats, inventory, zone, online}).
+-record(goblet_player,{name, id, title, appearance, role, stats, inventory, online}).
 -record(goblet_entity,{id, name, stats, scripts}).
 -record(goblet_object,{id, zone, coordinates}).
 -record(goblet_script,{id, name, content, scope}).
 -record(goblet_zone,{id, name, shortname}).
+
+% Records representing ephemeral objects, such as matches
+-record(goblet_match, {id=-1, state, players, players_max, start_time, mode, extra= <<>>}).
