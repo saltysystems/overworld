72b81ad2eebbd0ace5ba9ac6d96cd882db3ae318
Lincoln Bryant <cmdrk@protonmail.com>
2021-03-12 23:26:23 -0600

    added state to the session, added lobby info to the protocol decoding

--- apps/goblet/src/goblet_protocol.erl
+++ apps/goblet/src/goblet_protocol.erl
@@ -1,6 +1,6 @@
 -module(goblet_protocol).
 
--export([decode/2, account_new/1, account_login/1, player_new/2]).
+-export([decode/2, account_new/1, account_login/1, player_new/2, lobby_info/2]).
 -export([player_log/1]).
 
 -include("goblet_opcode.hrl").
@@ -9,7 +9,7 @@
 
 -include_lib("kernel/include/logger.hrl").
 
--record(session, {email = none, authenticated = false, player = none}).
+-record(session, {email = none, authenticated = false, statem = login}).
 
 %%============================================================================
 %% Goblet Protocol.
@@ -33,6 +33,9 @@ decode(<<OpCode:16, T/binary>>, _State) when OpCode =:= ?ACCOUNT_NEW ->
 decode(<<OpCode:16, T/binary>>, _State) when OpCode =:= ?ACCOUNT_LOGIN ->
     logger:notice("Got an account login request~n"),
     account_login(T);
+decode(<<OpCode:16, T/binary>>, State) when OpCode =:= ?LOBBY_INFO ->
+    logger:notice("Got a lobby info request~n"),
+    lobby_info(T, State);
 decode(<<OpCode:16, T/binary>>, State) when OpCode =:= ?PLAYER_NEW ->
     logger:notice("Got a new player request~n"),
     % Need to inspect the state for acct info
@@ -69,10 +72,10 @@ account_new(Message) ->
                     status = 'ERROR',
                     error = atom_to_list(Error)
                 }),
-                {Reply, #session{authenticated = false}};
+                {Reply, #session{authenticated = false, statem = lobby}};
             _ ->
                 Reply = goblet_pb:encode_msg(#'AccountNewResp'{status = 'OK'}),
-                {Reply, #session{email = Email, authenticated = true}}
+                {Reply, #session{email = Email, authenticated = true, statem = lobby}}
         end,
     OpCode = <<?ACCOUNT_NEW:16>>,
     {[OpCode, Msg], NewState}.
@@ -92,7 +95,7 @@ account_login(Message) ->
                 Record = goblet_db:account_by_email(Email),
                 Players = Record#goblet_account.player_ids,
                 Reply = goblet_pb:encode_msg(#'AccountLoginResp'{status = 'OK', players = Players}),
-                {Reply, Email};
+                {Reply, #session{email = Email, authenticated = true, statem = lobby}};
             false ->
                 Reply = goblet_pb:encode_msg(#'AccountLoginResp'{
                     status = 'ERROR',
@@ -102,6 +105,19 @@ account_login(Message) ->
         end,
     OpCode = <<?ACCOUNT_LOGIN:16>>,
     {[OpCode, Msg], NewState}.
+
+%%----------------------------------------------------------------------------
+%% @doc Get the current lobby information
+%% @end
+%%----------------------------------------------------------------------------
+-spec lobby_info(binary(), any()) -> {[binary(), ...], any()}.
+lobby_info(_Message, State) ->
+    Matches = goblet_lobby:get_matches(),
+    M1 = [repack_match(X) || X <- Matches],
+    Resp = #'ResponseObject'{status = 'OK'},
+    Msg = goblet_pb:encode_msg(#'LobbyInfo'{resp = Resp, matches = M1}),
+    OpCode = <<?LOBBY_INFO:16>>,
+    {[OpCode, Msg], State}.
 
 %%----------------------------------------------------------------------------
 %% @doc Create a new player character
@@ -134,3 +150,15 @@ sanitize_message(Message) ->
     %TODO: Check for message lengths, etc. Ensure that a client isn't DOSing
     %      other client(s)
     Message.
+
+repack_match(Match) ->
+    % This is pointless and stupid.
+    #'Match'{
+        id = Match#goblet_match.id,
+        state = Match#goblet_match.state,
+        players = Match#goblet_match.players,
+        players_max = Match#goblet_match.players_max,
+        start_time = Match#goblet_match.start_time,
+        mode = Match#goblet_match.mode,
+        extra = Match#goblet_match.extra
+    }.
