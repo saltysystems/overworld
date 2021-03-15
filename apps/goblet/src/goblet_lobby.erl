d9d77cd5255bd9a063e464f6c7101e5213cf4d94
cmdrk <cmdrk@protonmail.com>
2021-03-14 20:59:08 -0500

    renamed some functions

--- apps/goblet/src/goblet_lobby.erl
+++ apps/goblet/src/goblet_lobby.erl
@@ -6,7 +6,11 @@
 %% API
 -export([start/0, stop/0]).
 
--export([get_matches/0, add_match/1, del_match/1]).
+-export([
+    get_matches/0, 
+    create_match/3, 
+    delete_match/1
+]).
 
 %% gen_server callbacks
 -export([
@@ -40,17 +44,17 @@ get_matches() ->
 %% @doc Add a match to the lobby server, return a record of the match
 %%      with updated fields (state, ID).
 %%-------------------------------------------------------------------
--spec add_match(tuple()) -> {ok, tuple()}.
-add_match(MatchInfo) ->
-    gen_server:call(?MODULE, {add_match, MatchInfo}).
+-spec create_match(atom(),pos_integer(),binary()) -> {ok, tuple()}.
+create_match(Mode, MaxPlayers, Extra) ->
+    gen_server:call(?MODULE, {create_match, Mode, MaxPlayers, Extra}).
 
 %%-------------------------------------------------------------------
 %% @doc Delete the match from the lobby server, if it exists. If
 %%      it does not, then do nothing.
 %%-------------------------------------------------------------------
--spec del_match(tuple()) -> ok.
-del_match(MatchInfo) ->
-    gen_server:call(?MODULE, {del_match, MatchInfo}).
+-spec delete_match(tuple()) -> ok.
+delete_match(MatchInfo) ->
+    gen_server:call(?MODULE, {delete_match, MatchInfo}).
 
 %%===================================================================
 %% gen_server Callbacks
@@ -62,16 +66,20 @@ init([]) ->
 
 handle_call(get_matches, _From, {ID, MatchList}) ->
     {reply, MatchList, {ID, MatchList}};
-handle_call({add_match, NewMatch}, _From, {ID, Matches}) ->
-    MatchState = "creating",
+handle_call({create_match, Mode, MaxPlayers, Extra}, _From, {ID, Matches}) ->
+    MatchState = 'CREATING',
     StartTime = erlang:system_time(),
-    Match = NewMatch#goblet_match{id = ID, state = MatchState, start_time = StartTime},
+    Match = #goblet_match{id = ID, 
+                            state = MatchState, 
+                            mode = Mode,
+                            players_max = MaxPlayers,
+                            start_time = StartTime,
+                            extra = Extra},
     MatchList = [Match | Matches],
     Reply = {ok, Match},
-
     % Reply with the match list, increment the next available MatchID
     {reply, Reply, {ID + 1, MatchList}};
-handle_call({del_match, MatchInfo}, _From, {NextID, Matches}) ->
+handle_call({delete_match, MatchInfo}, _From, {NextID, Matches}) ->
     ID = MatchInfo#goblet_match.id,
     NewMatches = lists:keydelete(ID, #goblet_match.id, Matches),
     {reply, ok, {NextID, NewMatches}};
