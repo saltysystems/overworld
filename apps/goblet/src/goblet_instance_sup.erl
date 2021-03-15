eb31dd69bff4bacbb889a3667d2e500c5b0028e7
cmdrk <cmdrk@protonmail.com>
2021-03-14 21:00:23 -0500

    apps/goblet/src/goblet_instance_sup.erl
    supervisor for instances

---
+++ apps/goblet/src/goblet_instance_sup.erl
@@ -0,0 +1,23 @@
+-module(goblet_instance_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/1]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link([]) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+init([]) ->
+    SupFlags = #{strategy => simple_one_for_one,
+                 intensity => 0,
+                 period => 1},
+	ChildSpecs = [#{id => goblet_instance_server,
+                    start => {goblet_instance_server, start_link, [1]},
+                    shutdown => brutal_kill}],
+    {ok, {SupFlags, ChildSpecs}}.
