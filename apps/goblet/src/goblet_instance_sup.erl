3178f2c532e3f204c18def43726691e0ee5feb16
cmdrk <cmdrk@protonmail.com>
2021-03-21 00:15:49 -0500

    take no arguments for start_link else OTP will be upset

--- apps/goblet/src/goblet_instance_sup.erl
+++ apps/goblet/src/goblet_instance_sup.erl
@@ -3,21 +3,27 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/1]).
+-export([start_link/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
-start_link([]) ->
+start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
-    SupFlags = #{strategy => simple_one_for_one,
-                 intensity => 0,
-                 period => 1},
-	ChildSpecs = [#{id => goblet_instance_server,
-                    start => {goblet_instance_server, start_link, [1]},
-                    shutdown => brutal_kill}],
+    SupFlags = #{
+        strategy => simple_one_for_one,
+        intensity => 0,
+        period => 1
+    },
+    ChildSpecs = [
+        #{
+            id => goblet_instance_server,
+            start => {goblet_instance_server, start, []},
+            shutdown => brutal_kill
+        }
+    ],
     {ok, {SupFlags, ChildSpecs}}.
