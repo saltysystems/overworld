c5f3e1225dd23dcfde0d14a15f7fe3af3525bf4b
cmdrk <cmdrk@protonmail.com>
2021-03-21 00:17:26 -0500

    launch the instance supervisor with the lobby server

--- apps/goblet/src/goblet_sup.erl
+++ apps/goblet/src/goblet_sup.erl
@@ -27,13 +27,13 @@ start_link() ->
 %%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{
-        strategy => one_for_all,
-        intensity => 0,
-        period => 1
+        strategy => one_for_one,
+        intensity => 1,
+        period => 5
     },
     ChildSpecs = [
-        #{id => goblet_lobby, start => {goblet_lobby, start, []}}
-        %#{id => goblet_instance_sup, start => {goblet_instance_sup, start_link, []}}
+        #{id => goblet_lobby, start => {goblet_lobby, start, []}},
+        #{id => goblet_instance_sup, start => {goblet_instance_sup, start_link, []}}
     ],
     {ok, {SupFlags, ChildSpecs}}.
 
