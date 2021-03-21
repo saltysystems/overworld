ec1b8773401b31d9dfde96f56a3ca4efdf4e1154
Lincoln Bryant <cmdrk@protonmail.com>
2021-03-21 00:16:21 -0500

    try to abstract some of the functions that currently process a list and add a few new functions

--- apps/goblet/src/goblet_lobby.erl
+++ apps/goblet/src/goblet_lobby.erl
@@ -7,8 +7,10 @@
 -export([start/0, stop/0]).
 
 -export([
-    get_matches/0, 
-    create_match/3, 
+    get_matches/0,
+    create_match/2,
+    create_match/3,
+    start_match/1,
     delete_match/1
 ]).
 
@@ -44,15 +46,27 @@ get_matches() ->
 %% @doc Add a match to the lobby server, return a record of the match
 %%      with updated fields (state, ID).
 %%-------------------------------------------------------------------
--spec create_match(atom(),pos_integer(),binary()) -> {ok, tuple()}.
+-spec create_match(atom(), pos_integer()) -> {ok, tuple()} | {error, atom()}.
+create_match(Mode, MaxPlayers) ->
+    create_match(Mode, MaxPlayers, <<>>).
+
+-spec create_match(atom(), pos_integer(), binary()) -> {ok, tuple()} | {error, atom()}.
 create_match(Mode, MaxPlayers, Extra) ->
     gen_server:call(?MODULE, {create_match, Mode, MaxPlayers, Extra}).
+
+%%-------------------------------------------------------------------
+%% @doc Add a match to the lobby server, return a record of the match
+%%      with updated fields (state, ID).
+%%-------------------------------------------------------------------
+-spec start_match(pos_integer()) -> {ok, tuple()} | {error, tuple()}.
+start_match(MatchID) ->
+    gen_server:call(?MODULE, {start_match, MatchID}).
 
 %%-------------------------------------------------------------------
 %% @doc Delete the match from the lobby server, if it exists. If
 %%      it does not, then do nothing.
 %%-------------------------------------------------------------------
--spec delete_match(tuple()) -> ok.
+-spec delete_match(pos_integer()) -> ok.
 delete_match(MatchInfo) ->
     gen_server:call(?MODULE, {delete_match, MatchInfo}).
 
@@ -60,28 +74,40 @@ delete_match(MatchInfo) ->
 %% gen_server Callbacks
 %%===================================================================
 init([]) ->
-    MatchList = [],
+    Matches = [],
     NextID = 0,
-    {ok, {NextID, MatchList}}.
+    {ok, {NextID, Matches}}.
 
-handle_call(get_matches, _From, {ID, MatchList}) ->
-    {reply, MatchList, {ID, MatchList}};
+handle_call(get_matches, _From, {NextID, Matches}) ->
+    {reply, Matches, {NextID, Matches}};
 handle_call({create_match, Mode, MaxPlayers, Extra}, _From, {ID, Matches}) ->
     MatchState = 'CREATING',
     StartTime = erlang:system_time(),
-    Match = #goblet_match{id = ID, 
-                            state = MatchState, 
-                            mode = Mode,
-                            players_max = MaxPlayers,
-                            start_time = StartTime,
-                            extra = Extra},
+    Match = #goblet_match{
+        id = ID,
+        state = MatchState,
+        mode = Mode,
+        players_max = MaxPlayers,
+        start_time = StartTime,
+        extra = Extra
+    },
     MatchList = [Match | Matches],
     Reply = {ok, Match},
     % Reply with the match list, increment the next available MatchID
     {reply, Reply, {ID + 1, MatchList}};
-handle_call({delete_match, MatchInfo}, _From, {NextID, Matches}) ->
-    ID = MatchInfo#goblet_match.id,
-    NewMatches = lists:keydelete(ID, #goblet_match.id, Matches),
+handle_call({start_match, MatchID}, _From, {NextID, Matches}) ->
+    Match = match_find(MatchID, Matches),
+    {Reply, UpdatedMatches} =
+        case maybe_start(Match, Matches) of
+            {Error, []} ->
+                {Error, Matches};
+            {R, U} ->
+                {R, U}
+        end,
+    {reply, Reply, {NextID, UpdatedMatches}};
+handle_call({delete_match, MatchID}, _From, {NextID, Matches}) ->
+    Match = match_find(MatchID, Matches),
+    NewMatches = match_del(Match, Matches),
     {reply, ok, {NextID, NewMatches}};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
@@ -97,3 +123,26 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+% Internal functions that may be subject to change if the backend
+% implementation were to change from lists to e.g. ETS
+
+match_find(MatchID, Matches) ->
+    lists:keyfind(MatchID, #goblet_match.id, Matches).
+
+match_del(Match, Matches) when Match == false ->
+    % simply return the existing list of matches
+    Matches;
+match_del(Match, Matches) ->
+    MatchID = Match#goblet_match.id,
+    lists:keydelete(MatchID, #goblet_match.id, Matches).
+
+maybe_start(Match, Matches) when Match == false ->
+    {{error, no_such_match}, Matches};
+maybe_start(Match, Matches) when length(Match#goblet_match.players) == 0 ->
+    {{error, empty_match}, Matches};
+maybe_start(Match, Matches) ->
+    supervisor:start_child(goblet_instance_sup, [Match#goblet_match.players, Match#goblet_match.id]),
+    UpdatedMatch = Match#goblet_match{state = 'PLAYING'},
+    lists:keyreplace(Match#goblet_match.id, #goblet_match.id, Matches, UpdatedMatch),
+    {ok, started}.
