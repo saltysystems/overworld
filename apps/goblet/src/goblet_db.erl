7d26c025939927ae9c986a9638445da6e9e28dc8
Lincoln Bryant <cmdrk@protonmail.com>
2021-03-25 00:32:34 -0500

    formatting

--- apps/goblet/src/goblet_db.erl
+++ apps/goblet/src/goblet_db.erl
@@ -190,75 +190,68 @@ valid_password(Password, Hash, Salt) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 create_get_account_test() ->
-	% Create an account
-	Email = "TestUser@doesntexist.notadomain",
+    % Create an account
+    Email = "TestUser@doesntexist.notadomain",
     Password = "TestPassword1234",
-	Resp = goblet_db:create_account(Email, Password),
-	?assertEqual(ok, Resp),
+    Resp = goblet_db:create_account(Email, Password),
+    ?assertEqual(ok, Resp),
 
-	% Get the account by name
-	Resp1 = goblet_db:account_by_email(Email),
-	?assertEqual(Email, Resp1#goblet_account.email),
-	?assert(Resp1#goblet_account.id > 0),
+    % Get the account by name
+    Resp1 = goblet_db:account_by_email(Email),
+    ?assertEqual(Email, Resp1#goblet_account.email),
+    ?assert(Resp1#goblet_account.id > 0),
 
-	% Try to make an account that already exists
-	RespAgain = goblet_db:create_account(Email, Password),
-	?assertEqual({error, email_already_registered}, RespAgain).
-
+    % Try to make an account that already exists
+    RespAgain = goblet_db:create_account(Email, Password),
+    ?assertEqual({error, email_already_registered}, RespAgain).
 
 account_login_fake_test() ->
-	Email = "nobody@notadomain",
+    Email = "nobody@notadomain",
     Password = "TestPassword1234",
-	Resp = goblet_db:account_login(Email, Password),
-	?assertEqual({error, no_such_account}, Resp).
-
+    Resp = goblet_db:account_login(Email, Password),
+    ?assertEqual({error, no_such_account}, Resp).
 
 account_login_test() ->
-	Email = "TestUser@doesntexist.notadomain",
+    Email = "TestUser@doesntexist.notadomain",
     Password = "TestPassword1234",
-	Resp = goblet_db:account_login(Email, Password),
-	?assertEqual(true, Resp).
-
+    Resp = goblet_db:account_login(Email, Password),
+    ?assertEqual(true, Resp).
 
 account_login_badpass_test() ->
-	Email = "TestUser@doesntexist.notadomain",
+    Email = "TestUser@doesntexist.notadomain",
     Password = "thewrongpassword",
-	Resp = goblet_db:account_login(Email, Password),
-	?assertEqual(false, Resp).
-
+    Resp = goblet_db:account_login(Email, Password),
+    ?assertEqual(false, Resp).
 
 create_player_test() ->
-	Email = "TestUser@doesntexist.notadomain",
-	Name = "Chester McTester",
-	Title = "Fleet Captain",
-	Appearance = 1,
-	Role = "Destroyer",
-	Resp = goblet_db:create_player(Name, Title, Appearance, Role, Email),
-	?assertEqual(ok, Resp),
-	% Now check to see that the player is properly associated with the 
-	Acct = goblet_db:account_by_email(Email),
-	?assertEqual(["Chester McTester"],Acct#goblet_account.player_ids).
-
+    Email = "TestUser@doesntexist.notadomain",
+    Name = "Chester McTester",
+    Title = "Fleet Captain",
+    Appearance = 1,
+    Role = "Destroyer",
+    Resp = goblet_db:create_player(Name, Title, Appearance, Role, Email),
+    ?assertEqual(ok, Resp),
+    % Now check to see that the player is properly associated with the
+    Acct = goblet_db:account_by_email(Email),
+    ?assertEqual(["Chester McTester"], Acct#goblet_account.player_ids).
 
 delete_player_test() ->
-	Email = "TestUser@doesntexist.notadomain",
-	Name = "Chester McTester",
-	Resp = goblet_db:delete_player(Name, Email),
-	?assertEqual(ok, Resp).
-
+    Email = "TestUser@doesntexist.notadomain",
+    Name = "Chester McTester",
+    Resp = goblet_db:delete_player(Name, Email),
+    ?assertEqual(ok, Resp).
 
 delete_account_test() ->
-	% Delete an account
-	Email = "TestUser@doesntexist.notadomain",
-	Resp = goblet_db:delete_account(Email),
-	?assertEqual(ok, Resp),
-	% should always succeed when deleting
-	RespAgain = goblet_db:delete_account(Email),
-	?assertEqual(ok, RespAgain).
-
+    % Delete an account
+    Email = "TestUser@doesntexist.notadomain",
+    Resp = goblet_db:delete_account(Email),
+    ?assertEqual(ok, Resp),
+    % should always succeed when deleting
+    RespAgain = goblet_db:delete_account(Email),
+    ?assertEqual(ok, RespAgain).
 
 account_by_email_no_acct_test() ->
-	% Try to get the record for an account that doesn't exist
-	Email = "TestUser@doesntexist.notadomain",
-	Resp = goblet_db:account_by_email(Email),
-	?assertEqual({error, no_such_account}, Resp).
+    % Try to get the record for an account that doesn't exist
+    Email = "TestUser@doesntexist.notadomain",
+    Resp = goblet_db:account_by_email(Email),
+    ?assertEqual({error, no_such_account}, Resp).
