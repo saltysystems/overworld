ef6f7ce4d0ce2de0250b69b091d8d943c7f3e605
cmdrk <cmdrk@protonmail.com>
2021-03-27 22:52:33 -0500

    add some basic items at startup so we can do something

--- apps/goblet/src/goblet_app.erl
+++ apps/goblet/src/goblet_app.erl
@@ -10,6 +10,31 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    % Try to setup items
+    goblet_db:create_item("Reactor MK I", 1, move, 'SELF', 0, 0, none, 100),
+    goblet_db:create_item(
+        "Missile MK I",
+        2,
+        missile,
+        'DIRECT',
+        40,
+        0,
+        none,
+        100
+    ),
+    goblet_db:create_item("Beam MK I", 4, beam, 'LINEAR', 60, 0, none, 100),
+    goblet_db:create_item(
+        "Repair MK I",
+        2,
+        repair,
+        'DIRECT',
+        0,
+        25,
+        none,
+        100
+    ),
+    goblet_db:create_item("Scanner MK I", 1, scan, 'AREA', 0, 0, none, 100),
+
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/ws", goblet_websocket, []}
