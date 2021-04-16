2fb201e4886437b19eb050641ee048f2289cb255
cmdrk <cmdrk@protonmail.com>
2021-04-15 21:55:42 -0500

    changed db record

--- apps/goblet/include/goblet_database.hrl
+++ apps/goblet/include/goblet_database.hrl
@@ -5,7 +5,7 @@
 
 % Records representing the database tables in Mnesia
 -record(goblet_account,{email, id, hash, salt, first_login, last_login, player_ids}).
--record(goblet_player,{name, id, appearance, role, health, status_effects, inventory}).
+-record(goblet_player,{name, id, colors, symbols, role, health, status_effects, inventory}).
 % separate table because separate IDs
 -record(goblet_entity,{name, id, appearance, role, health, status_effects, inventory}).
 -record(goblet_item, {name, id, ap, action, target_type, target_damage, target_health, status_effect, price}).
