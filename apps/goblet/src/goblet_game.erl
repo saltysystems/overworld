1eec50331c5a8c008bcdf4733c4dcd6d32da97c3
cmdrk <cmdrk@protonmail.com>
2021-05-04 00:24:15 -0500

    reduce the nume rof items we give players. just start with reactor

--- apps/goblet/src/goblet_game.erl
+++ apps/goblet/src/goblet_game.erl
@@ -10,6 +10,11 @@
     initialize_board/3
 ]).
 
+-include_lib("kernel/include/logger.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-record(action, {ap, name, from, target}).
+
 %======================================================================
 % Public API
 %======================================================================
@@ -28,11 +33,7 @@ new_player(Name, Colors, Symbols, Role, Account) ->
             goblet_db:create_player(Name, Colors, Symbols, Role, Account),
             % Now give the player some basic inventory
             % TODO: replace me with a more dynamic system
-            goblet_db:item_to_player("Reactor MK I", Name),
-            goblet_db:item_to_player("Missile MK I", Name),
-            goblet_db:item_to_player("Beam MK I", Name),
-            goblet_db:item_to_player("Repair MK I", Name),
-            goblet_db:item_to_player("Scanner MK I", Name);
+            goblet_db:item_to_player("Reactor MK I", Name);
         Error ->
             {error, Error}
     end.
@@ -53,9 +54,59 @@ initialize_board(Board, [Player | Rest]) ->
             initialize_board(B1, Rest)
     end.
 
-%======================================================================
-% Internal Functions
-%======================================================================
+calculate_round(_Actions, Board) ->
+    {[], Board}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Internal Functions                                                 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+normalize_actions(ActionList) ->
+    % Take a list of actions for a player, e.g.
+    %    [{1,shoot}, {2, move, {4, laz0r}]
+    % and then normalize it such that the AP costs are translated into Phases:
+    %  [{1,shoot}, {3, move}, {7, laz0r}]
+    normalize_actions(ActionList, []).
+
+normalize_actions([], Acc) ->
+    lists:reverse(Acc);
+normalize_actions([H | T], []) ->
+    normalize_actions(T, [H]);
+normalize_actions([H | T], [AccH | AccT]) when is_record(H, action) ->
+    MP = H#action.ap + AccH#action.ap,
+    MPList = H#action{ap = MP},
+    normalize_actions(T, [MPList | [AccH | AccT]]);
+normalize_actions([H | T], [AccH | AccT]) ->
+    % H = 1, T = [2,3,4] ; AccH = 1, AccT = []
+    NewList = H + AccH,
+    normalize_actions(T, [NewList | [AccH | AccT]]).
+
+% The ActionList must already do the math needed to handle this
+phases(ActionList) ->
+    % Get the largest key
+    % AP must be the first item in the record, hence 2nd key
+    S = lists:keysort(2, ActionList),
+    % Get the last element of the list
+    MaxPhase = lists:last(S),
+    % Recurse through the phases, pairing up matching keys into the same
+    % phase til we hit 0.
+    group_phases(MaxPhase#action.ap, S).
+
+group_phases(N, KeyList) ->
+    group_phases(N, KeyList, []).
+
+group_phases(N, _KeyList, Acc) when N == 0 ->
+    Acc;
+group_phases(N, KeyList, Acc) ->
+    %PhaseGroup = [ X || {_, N1, _, _} = X <- KeyList, N == N1],
+    PhaseGroup = [X || #action{ap = N1} = X <- KeyList, N == N1],
+    case PhaseGroup of
+        [] ->
+            group_phases(N - 1, KeyList, Acc);
+        _ ->
+            group_phases(N - 1, KeyList, [PhaseGroup | Acc])
+    end.
+
 is_valid_name(Name) when length(Name) < 64 ->
     case goblet_db:player_by_name(Name) of
         {error, no_such_player} -> ok;
@@ -82,3 +133,42 @@ is_valid_role(Role) ->
 %======================================================================
 % Tests
 %======================================================================
+normalize_actions_test() ->
+    Actions = [
+        #action{name = shoot, ap = 2, from = "player1", target = "player2"},
+        #action{name = move, ap = 1, from = "player1", target = {0, 0}},
+        #action{name = laz0r, ap = 6, from = "player2", target = "player1"}
+    ],
+    % Convert AP to Phases
+    MP = normalize_actions(Actions),
+    Last = lists:last(MP),
+    ?assertEqual(9, Last#action.ap).
+
+phases_test() ->
+    P1_Actions = [
+        #action{name = shoot, ap = 2, from = "player1", target = "player2"},
+        #action{name = move, ap = 1, from = "player1", target = {0, 0}},
+        #action{name = laz0r, ap = 6, from = "player1", target = "player2"}
+    ],
+    P2_Actions = [
+        #action{name = move, ap = 1, from = "player2", target = {1, 1}},
+        #action{name = shoot, ap = 2, from = "player2", target = "player1"},
+        #action{
+            name = shield,
+            ap = 3,
+            from = "player2",
+            target = "player1"
+        },
+        #action{
+            name = missile,
+            ap = 3,
+            from = "player2",
+            target = "player1"
+        }
+    ],
+    % Convert AP to Phases
+    MP = normalize_actions(P1_Actions),
+    MP2 = normalize_actions(P2_Actions),
+    Collected = MP ++ MP2,
+    % Group the phases
+    phases(Collected).
