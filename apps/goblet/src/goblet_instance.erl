6984e7584eb0395aabe3b868a0f4704d49ed7f90
Lincoln Bryant <cmdrk@protonmail.com>
2021-05-08 10:01:45 -0500

    tried to clarify the logging a bit. added event handlers for board state and player list requests

--- apps/goblet/src/goblet_instance.erl
+++ apps/goblet/src/goblet_instance.erl
@@ -5,19 +5,23 @@
 -behaviour(gen_statem).
 
 % Public API
--export([start/2, player_ready/2, player_decision/3, get_board_state/1]).
+-export([start/2, 
+        player_ready/2, 
+        player_decision/3, 
+        get_board_state/1, 
+        get_players/1
+]).
 
-% Callbacks
+% Required gen_statem callbacks
 -export([init/1, callback_mode/0, terminate/3]).
 
 % State Functions
 % Do not use directly
 -export([
-    prepare/3,
-    decision/3,
-    execution/3,
-    %finish/4,
-    board_state/3
+    prepare_phase/3,
+    decision_phase/3,
+    execution_phase/3
+    %finish_phase/4,
 ]).
 
 -define(SERVER(Name), {via, gproc, {n, l, {?MODULE, Name}}}).
@@ -43,6 +47,9 @@ player_decision(Player, Actions, MatchID) ->
 get_board_state(MatchID) ->
     gen_statem:call(?SERVER(MatchID), board_state).
 
+get_players(MatchID) ->
+    gen_statem:call(?SERVER(MatchID), player_list).
+
 % Callbacks
 init({PlayerList, MatchID}) ->
     M = #match{
@@ -51,27 +58,22 @@ init({PlayerList, MatchID}) ->
         readyplayers = [],
         board = goblet_game:initialize_board(1, 1, PlayerList)
     },
-    {ok, prepare, M}.
+    {ok, prepare_phase, M}.
 
 callback_mode() ->
     state_functions.
 
-% State Functions
-board_state({call, From}, board_state, Data) ->
-    Board = Data#match.board,
-    {keep_state, {reply, From, Board}}.
-
 % In prepare stage, we wait for all Players to confirm that they are ready.
-prepare(
+prepare_phase(
     cast,
     {prepare, Player},
-    #match{id = _ID, playerlist = PlayerList, readyplayers = Ready} = Data
+    Data
 ) ->
+    #match{id = ID, playerlist = PlayerList, readyplayers = Ready} = Data,
     ReadyPlayers = [Player | Ready],
-    % TODO: Validate that Ready is a subset of Player
     ReadySet = sets:from_list(ReadyPlayers),
     PlayerSet = sets:from_list(PlayerList),
-    logger:notice("Player ~p is now ready.", [Player]),
+    logger:notice("Player ~p is now ready in Match ~p.", [Player, ID]),
     NextState =
         case PlayerSet == ReadySet of
             true ->
@@ -79,38 +81,42 @@ prepare(
                 % next phase. We want a maximum of 20s to go by before we
                 % skip over decision and go straight to execute
                 logger:notice(
-                    "All players are ready, move on to decision state"
+                    "(Prepare) All players are ready. -> Decision"
                 ),
                 TimeOut = {{timeout, decide}, 20000, execute},
-                {next_state, decision, Data#match{readyplayers = []}, [
-                    TimeOut
-                ]};
+                {next_state, decision_phase, Data#match{readyplayers = []},
+                    [
+                        TimeOut
+                    ]};
             false ->
-                % Wait for all players to be ready
-                % TODO: Implement timeout
-                {next_state, prepare, Data#match{
+                % Not all players are ready, stay in the prepare phase
+                % indefinitely
+                {next_state, prepare_phase, Data#match{
                     readyplayers = ReadyPlayers
                 }}
         end,
-    logger:notice("Ready players are:~p", [ReadyPlayers]),
-
+    logger:notice("(Prepare) Match ~p: ~p of ~p players are ready", [
+        ID,
+        sets:size(ReadySet),
+        sets:size(PlayerSet)
+    ]),
     NextState;
-prepare(EventType, EventContent, Data) ->
-    logger:warning("Got a bad event sent to prepare"),
+prepare_phase(EventType, EventContent, Data) ->
     handle_event(EventType, EventContent, Data).
 
 % In the decision stage, we accept decisions from players until the timeout is
 % reached. Then we move to Execute phase.
-decision(
+decision_phase(
     cast,
     {decision, Player, PlayerActions},
-    #match{playerlist = PL, readyplayers = RP, actions = Actions} = Data
+    Data
 ) ->
+    #match{playerlist = PL, readyplayers = RP, actions = Actions} = Data,
     % TODO: Validate action list
     Ready = [Player | RP],
     RSet = sets:from_list(RP),
     PSet = sets:from_list(PL),
-    logger:notice("Player ~p has made a decision.", [Player]),
+    logger:notice("(Decision) Player ~p has made a decision.", [Player]),
     % Update the match state with the player's decision
     Data1 = Data#match{actions = [PlayerActions | Actions]},
     NextState =
@@ -120,23 +126,25 @@ decision(
                 % Cancel the decision timer by setting it to infinity and
                 % move onto execute state
                 logger:notice(
-                    "All players are ready, move on to execution state"
+                    "(Decision) All players are ready. -> Execution"
                 ),
                 TimeOut = {{timeout, decide}, infinity, execute},
-                {next_state, execution, Data1#match{readyplayers = []}, [
-                    TimeOut
-                ]};
+                {next_state, execution_phase,
+                    Data1#match{readyplayers = []}, [
+                        TimeOut
+                    ]};
             false ->
-                {next_state, decision, Data1#match{readyplayers = Ready}}
+                {next_state, decision_phase, Data1#match{
+                    readyplayers = Ready
+                }}
         end,
-    logger:notice("Ready players are: ~p", [Ready]),
+    logger:notice("(Decision) Ready players are: ~p", [Ready]),
     NextState;
-decision({timeout, decide}, execute, Data) ->
+decision_phase({timeout, decide}, execute, Data) ->
     % Decisions have timed out, move on
-    logger:notice("10000ms has expired - force move to execution state"),
-    {next_state, execution, Data, [{state_timeout, 10000, decide}]};
-decision(EventType, EventContent, Data) ->
-    logger:warning("Got a bad event sent to decision"),
+    logger:notice("(Decision) 20000ms have elapsed. -> Execution"),
+    {next_state, execution_phase, Data, [{state_timeout, 10000, decide}]};
+decision_phase(EventType, EventContent, Data) ->
     handle_event(EventType, EventContent, Data).
 
 % In the execution phase, we group actions, calculate the results, and give
@@ -144,24 +152,29 @@ decision(EventType, EventContent, Data) ->
 % clients have finished with their animations, or we time out after some
 % generous time period (60s ?). We check to see if the match is over, and go
 % to the Finish phase if it is. Otherwise, back to Decision.
-execution(
+execution_phase(
     state_timeout,
     decide,
-    #match{id = ID, board = B0, actions = A, playerlist = P} = Data
+    Data
 ) ->
+    #match{id = ID, board = B0, actions = A, playerlist = P} = Data,
     B1 = goblet_game:calculate_round(A, B0),
     goblet_protocol:match_state_update(B1, [], 'EXECUTE', P, [], ID),
-    logger:notice("60000ms has passed for animations, back to decision"),
+    logger:notice("(Execute) 10000ms have elapsed. -> Decison"),
     TimeOut = {{timeout, decide}, 20000, execute},
-    {next_state, decision, Data, [TimeOut]};
-execution(EventType, EventContent, Data) ->
-    logger:warning("Got a bad event sent to execution"),
+    {next_state, decision_phase, Data, [TimeOut]};
+execution_phase(EventType, EventContent, Data) ->
     handle_event(EventType, EventContent, Data).
 
-% In the finish phase, we instruct the clients to wrap up, show them results, and we shut down or wait to be shut down by our supervisor.
-
-handle_event(_EventType, _EventContent, Data) ->
-    {keep_state, Data}.
+handle_event({call, From}, board_state, #match{board = B} = Data) ->
+    {keep_state, Data, [{reply, From, B}]};
+handle_event({call, From}, player_list, #match{playerlist = P} = Data) ->
+    {keep_state, Data, [{reply, From, P}]};
+% Handle all other events
+handle_event(cast, _EventContent, Data) ->
+    {keep_state, Data};
+handle_event({call, From}, _EventContent, Data) ->
+    {keep_state, Data, [{reply, From, {error, unknown_handler}}]}.
 
 terminate(_Reason, _State, _Data) ->
     %TODO: Save the player progress
