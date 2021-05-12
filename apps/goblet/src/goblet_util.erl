f1f15e8176b143c622a46a9d1b07184613c121b9
Lincoln Bryant <cmdrk@protonmail.com>
2021-05-12 00:19:33 -0500

    new pipelining wrapper for mutating state down an assembly line of functions.

--- apps/goblet/src/goblet_util.erl
+++ apps/goblet/src/goblet_util.erl
@@ -1,5 +1,9 @@
 -module(goblet_util).
--export([run_checks/1, any_in_list/2]).
+-export([
+    run_checks/1,
+    any_in_list/2,
+    pipeline/2
+]).
 
 -spec run_checks(list()) -> ok | any().
 run_checks([]) ->
@@ -9,6 +13,10 @@ run_checks([H | T]) when is_function(H, 0) ->
         ok -> run_checks(T);
         Error -> Error
     end.
+
+% poor man's pipe
+pipeline(Input, Funs) ->
+    lists:foldl(fun(F, State) -> F(State) end, Input, Funs).
 
 any_in_list(L1, L2) ->
     % Checks to see if any item in L1 is present in L2
