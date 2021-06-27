679a970b3e44917ec2b97051e29edbfa52f60e62
Lincoln Bryant <cmdrk@protonmail.com>
2021-06-26 19:16:50 -0500

    addeda boolean version of the run checks function

--- apps/goblet/src/goblet_util.erl
+++ apps/goblet/src/goblet_util.erl
@@ -1,9 +1,19 @@
 -module(goblet_util).
 -export([
     run_checks/1,
+    run_bchecks/1,
     any_in_list/2,
     pipeline/2
 ]).
+
+-spec run_bchecks(list()) -> ok | any().
+run_bchecks([]) ->
+    ok;
+run_bchecks([H | T]) when is_function(H, 0) ->
+    case H() of
+        true -> run_checks(T);
+        _ -> false
+    end.
 
 -spec run_checks(list()) -> ok | any().
 run_checks([]) ->
