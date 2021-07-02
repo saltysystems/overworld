c87ce6d97927537dc7d7691416ee00483b27410c
cmdrk <cmdrk@protonmail.com>
2021-07-01 19:41:36 -0500

    add tcp nodelay for disabling Nagle's algorithm, add default flags to various items

--- apps/goblet/src/goblet_app.erl
+++ apps/goblet/src/goblet_app.erl
@@ -13,7 +13,17 @@
 
 start(_StartType, _StartArgs) ->
     % Try to setup items
-    goblet_db:create_item("Reactor MK I", 1, move, 'SELF', 0, 0, none, 100),
+    goblet_db:create_item(
+        "Reactor MK I",
+        1,
+        move,
+        'SELF',
+        0,
+        0,
+        none,
+        [visible],
+        100
+    ),
     goblet_db:create_item(
         "Missile MK I",
         2,
@@ -22,9 +32,20 @@ start(_StartType, _StartArgs) ->
         40,
         0,
         none,
+        [],
         100
     ),
-    goblet_db:create_item("Beam MK I", 4, beam, 'LINEAR', 60, 0, none, 100),
+    goblet_db:create_item(
+        "Beam MK I",
+        4,
+        beam,
+        'LINEAR',
+        60,
+        0,
+        none,
+        [],
+        100
+    ),
     goblet_db:create_item(
         "Repair MK I",
         2,
@@ -33,9 +54,20 @@ start(_StartType, _StartArgs) ->
         0,
         25,
         none,
+        [],
         100
     ),
-    goblet_db:create_item("Scanner MK I", 1, scan, 'AREA', 0, 0, none, 100),
+    goblet_db:create_item(
+        "Scanner MK I",
+        1,
+        scan,
+        'AREA',
+        0,
+        0,
+        none,
+        [],
+        100
+    ),
 
     Dispatch = cowboy_router:compile([
         {'_', [
@@ -44,7 +76,10 @@ start(_StartType, _StartArgs) ->
     ]),
     {ok, _} = cowboy:start_clear(
         http,
-        [{port, 4433}],
+        [
+            {port, 4433},
+            {nodelay, true}
+        ],
         #{env => #{dispatch => Dispatch}}
     ),
     goblet_sup:start_link().
