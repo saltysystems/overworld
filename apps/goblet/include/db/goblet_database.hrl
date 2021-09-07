51c490c75e88d4522bf461045626e7cfabf6478f
Lincoln Bryant <cmdrk@protonmail.com>
2021-09-07 00:43:14 -0500

    moved db and simplified some of the tables

--- apps/goblet/include/db/goblet_database.hrl
+++ apps/goblet/include/db/goblet_database.hrl
@@ -5,7 +5,6 @@
 
 % Records representing the database tables in Mnesia
 -record(goblet_account,{email, id, hash, salt, first_login, last_login, player_ids}).
--record(goblet_player,{name, id, colors, symbols, role, health, energy, flags, inventory}).
-% separate table because separate IDs
--record(goblet_mob,{name, id, appearance, type, health, energy, flags, inventory}).
+-record(goblet_player,{name, id, flags, inventory, shipgrid}).
+-record(goblet_mob,{name, id, flags, inventory, shipgrid, script}).
 -record(goblet_item, {name, id, ap, action, target_type, target_damage, target_health, status_effect, flags, price}).
