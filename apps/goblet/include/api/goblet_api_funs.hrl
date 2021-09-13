57ec8256cce7b721aa1c92474f85ee0da3ee2473
cmdrk <cmdrk@protonmail.com>
2021-09-13 00:27:20 -0500

    added some new functions for manipulating the database

--- apps/goblet/include/api/goblet_api_funs.hrl
+++ apps/goblet/include/api/goblet_api_funs.hrl
@@ -1,5 +1,10 @@
 -define(API_FUNS, 
    [
-    {"goblet.square", fun([X]) -> X * X end}
+    {"goblet.square", fun([X]) -> X * X end},
+    {"goblet.ship_component_new", fun([Name, Index, Type]) -> goblet_ship_component:new(Name, Index, list_to_atom(Type)) end},
+    {"goblet.ship_component_delete", fun([ID]) -> goblet_ship_component:delete(ID) end},
+    {"goblet.ship_component_wang_index", fun([ID]) -> goblet_ship_component:wang_index(ID) end},
+    {"goblet.ship_component_attributes", fun([ID]) -> goblet_ship_component:attributes(ID) end},
+    {"goblet.ship_component_name", fun([ID]) -> goblet_ship_component:name(ID) end}
    ]
  ).
