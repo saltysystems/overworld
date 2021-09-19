a4cbb152073d118d70025702827a6c505de3dc3f
cmdrk <cmdrk@protonmail.com>
2021-09-19 00:46:18 -0500

    refactored the scripting support a bit such that the API bindings are just defined in the source and moved things raound slightly

--- apps/goblet/src/gd_api/goblet_script_srv.erl
+++ apps/goblet/src/gd_api/goblet_script_srv.erl
@@ -2,8 +2,6 @@
 
 -behaviour(gen_server).
 
--include("api/goblet_api_funs.hrl").
-
 -define(SERVER(Name), {via, gproc, {n, l, {?MODULE, Name}}}).
 
 % public interface
@@ -46,7 +44,7 @@ do(What, Inspect, ID) ->
 init([]) ->
     % gdminus uses the process dictionary to store state !
     gdminus_int:init(),
-    setup_api(),
+    goblet_api:setup_api(),
     {ok, []}.
 
 handle_call({do, What, Inspect}, _From, State) ->
@@ -73,17 +71,3 @@ handle_info(_Info, State) -> {noreply, State}.
 terminate(_Reason, _State) -> ok.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
-
-%%---------------------------------------------------------------------------
-%% Internal functions
-%%---------------------------------------------------------------------------
-
-setup_api() ->
-    Funs = ?API_FUNS,
-    setup_api(Funs).
-
-setup_api([]) ->
-    ok;
-setup_api([{Name, Fun} | T]) ->
-    gdminus_int:insert_function(Name, Fun),
-    setup_api(T).
