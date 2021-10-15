9fe28622aceccccc38e48dbfd4b039903ef5aefe
cmdrk <cmdrk@protonmail.com>
2021-10-14 19:16:48 -0500

    rename

--- apps/goblet/src/lib/goblet_grid2d.erl
+++ apps/goblet/src/lib/goblet_grid2d.erl
@@ -1,4 +1,4 @@
--module(goblet_grid).
+-module(goblet_grid2d).
 
 -export([
     new/2, new/3,
@@ -17,7 +17,7 @@
     set_flag/3,
     unset_flag/3,
     has_flag/3,
-    grid_distance/2,
+    distance/2,
     get_coordinate_list/1
 ]).
 
@@ -164,8 +164,8 @@ has_flag(Coordinates, Flag, Map0) ->
     Tile = maps:get(Coordinates, Map0),
     maps:get(Flag, Tile#tile.flags, false).
 
--spec grid_distance(coords(), coords()) -> pos_integer().
-grid_distance({X1, Y1}, {X2, Y2}) ->
+-spec distance(coords(), coords()) -> pos_integer().
+distance({X1, Y1}, {X2, Y2}) ->
     abs(X2 - X1) + abs(Y2 - Y1).
 
 -spec get_coordinate_list(map()) -> list().
