2d9e89a0054b1cff716da624e169858fba5950a1
cmdrk <cmdrk@protonmail.com>
2022-09-10 12:39:23 -0500

    Add default constants

--- src/ow_app.erl
+++ src/ow_app.erl
@@ -9,6 +9,13 @@
 
 -export([start/2, stop/1]).
 
+-define(OVERWORLD_WEBSOCKET_PORT, 4434).
+-define(OVERWORLD_WEBSOCKET_TLS_PORT, 4435).
+-define(OVERWORLD_ENET_PORT, 4484).
+-define(OVERWORLD_ENET_DTLS_PORT, 4485).
+-define(OVERWORLD_ENET_CHANNEL_LIMIT, 1).
+-define(OVERWORLD_ENET_PEER_LIMIT, 4).
+
 % status information via JSON API
 -export([status/0]).
 
@@ -18,19 +25,24 @@ start(_StartType, _StartArgs) ->
             {"/ws", ow_websocket, []},
             {"/client/download", ow_dl_handler, []},
             {"/stats", ow_stats, []}
-            % TODO - See if this is needed, I think it's old.
-            %{"/libow.gd", cowboy_static,
-            %    {file, "apps/ow_core/static/libow.gd"}}
         ]}
     ]),
     {ok, _} = cowboy:start_clear(
         http,
         [
-            {port, 4433},
+            {port, ?OVERWORLD_WEBSOCKET_PORT},
             {nodelay, true}
         ],
         #{env => #{dispatch => Dispatch}}
     ),
+    % Enet Info
+    Port = ?OVERWORLD_ENET_PORT,
+    Handler = {ow_enet, start, []},
+    Options = [
+        {channel_limit, ?OVERWORLD_ENET_CHANNEL_LIMIT},
+        {peer_limit, ?OVERWORLD_ENET_PEER_LIMIT}
+    ],
+    {ok, _} = enet:start_host(Port, Handler, Options),
     ow_sup:start_link().
 
 stop(_State) ->
