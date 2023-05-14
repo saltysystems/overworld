d93e86c7f71eb43e7f492c3f37e109a9d069e895
cmdrk <cmdrk@proton.me>
2023-05-13 19:18:34 -0500

    first pass at protocol revamp

--- src/ow_protocol.erl
+++ src/ow_protocol.erl
@@ -14,13 +14,15 @@
 
 % public API
 -export([
-    start/1,
+    start/0,
     stop/0,
-    registered_apps/0,
-    registered_ops/0,
-    register/1,
-    register_app/1,
-    op_info/1,
+    register_rpc/1,
+    register_app/2,
+    apps/0,
+    app/1,
+    rpcs/0,
+    client_rpc/1,
+    server_rpc/1,
     decode/2,
     response/1,
     response/2
@@ -41,10 +43,6 @@
 -include_lib("kernel/include/logger.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--opaque opcode() :: <<_:16>>.
-
--export_type([opcode/0]).
-
 %%=========================================================================
 %% API
 %%=========================================================================
@@ -53,9 +51,9 @@
 %% @doc Start the gen_server
 %% @end
 %%-------------------------------------------------------------------------
--spec start([atom(), ...]) -> {ok, pid()} | ignore | {error, term()}.
-start(Modules) ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [Modules], []).
+-spec start() -> {ok, pid()} | ignore | {error, term()}.
+start() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 %%-------------------------------------------------------------------------
 %% @doc Stop the gen_server
@@ -72,10 +70,9 @@ stop() ->
 %%      State}} if the registration fails.
 %% @end
 %%-------------------------------------------------------------------------
--spec register_app(atom()) -> {reply, ok | {error, atom()}, map()}.
-register_app(Application) ->
-    logger:info("Registering application: ~p~n", [Application]),
-    gen_server:call(?MODULE, {register_app, Application}).
+-spec register_app(atom(), atom()) -> {reply, ok | {error, atom()}, map()}.
+register_app(Application, Encoder) ->
+    gen_server:call(?MODULE, {register_app, Application, Encoder}).
 
 %%-------------------------------------------------------------------------
 %% @doc Register a new opcode and associated callback function for Module.
@@ -83,10 +80,10 @@ register_app(Application) ->
 %%      the registration fails.
 %% @end
 %%-------------------------------------------------------------------------
--spec register(atom()) -> {reply, ok | {error, atom()}, map()}.
-register(Module) ->
+-spec register_rpc(atom()) -> {reply, ok | {error, atom()}, map()}.
+register_rpc(Module) ->
     logger:info("Registering callbacks for : ~p~n", [Module]),
-    gen_server:call(?MODULE, {register_op, Module}).
+    gen_server:call(?MODULE, {register_rpc, Module}).
 
 %%-------------------------------------------------------------------------
 %% @doc Decode messages from clients and route them to an appropriate
@@ -135,67 +132,84 @@ response(error, Msg) ->
     ).
 
 %%-------------------------------------------------------------------------
-%% @doc Get a list of opcodes registered with the server
+%% @doc Get a list of RPCs registered with the server
 %% @end
 %%-------------------------------------------------------------------------
--spec registered_ops() -> [pos_integer(), ...].
-registered_ops() ->
-    gen_server:call(?MODULE, registered_ops).
+-spec rpcs() -> {list(), list()}.
+rpcs() ->
+    gen_server:call(?MODULE, rpcs).
 
 %%-------------------------------------------------------------------------
-%% @doc Get a list of Overworld games registered with the server
+%% @doc Get a list of Overworld applications registered with the server
 %% @end
 %%-------------------------------------------------------------------------
--spec registered_apps() -> [atom(), ...].
-registered_apps() ->
-    gen_server:call(?MODULE, registered_apps).
+-spec apps() -> [atom(), ...].
+apps() ->
+    gen_server:call(?MODULE, apps).
 
 %%-------------------------------------------------------------------------
-%% @doc Return the MFA for a particular opcode
+%% @doc Get information for a particular registered app
 %% @end
 %%-------------------------------------------------------------------------
--spec op_info(pos_integer()) -> ow_rpc:rpc().
-op_info(OpCode) ->
-    gen_server:call(?MODULE, {op_info, OpCode}).
+-spec app(atom()) -> tuple().
+app(App) ->
+    gen_server:call(?MODULE, {app, App}).
+
+%%-------------------------------------------------------------------------
+%% @doc Return the info map for a particular opcode
+%% @end
+%%-------------------------------------------------------------------------
+-spec client_rpc(atom()) -> map().
+client_rpc(RPC) ->
+    gen_server:call(?MODULE, {client_rpc, RPC}).
+
+-spec server_rpc(atom()) -> map().
+server_rpc(RPC) ->
+    gen_server:call(?MODULE, {server_rpc, RPC}).
 
 %%============================================================================
 %% gen_server callbacks
 %%============================================================================
 
-init([Modules]) ->
-    % Register all functions defined in ?INITIAL_MODULES
-    InitialOps = lists:foldl(
-        fun(Item, Map) -> reg(Item, Map) end,
-        #{},
-        Modules
-    ),
-    InitialApps = [overworld],
-    InitialState = #{ops => InitialOps, apps => InitialApps},
-    {ok, InitialState}.
+init([]) ->
+    % Initialize the state
+    St0 = #{
+        % client RPCs
+        c_rpc => #{},
+        % server RPCs
+        s_rpc => #{},
+        apps => []
+    },
+    {ok, St0}.
 
-handle_call({register_op, Module}, _From, St0) ->
+handle_call({register_rpc, Module}, _From, St0) ->
     % Get list of opcodes to register for Module
-    Ops0 = maps:get(ops, St0),
-    Ops1 = reg(Module, Ops0),
-    {reply, ok, St0#{ops => Ops1}};
-handle_call({register_app, Application}, _From, St0) ->
+    %Ops0 = maps:get(ops, St0),
+    St1 = reg_rpc(Module, St0),
+    {reply, ok, St1};
+handle_call(
+    {register_app, Application, Encoder}, _From, #{apps := Apps} = St0
+) ->
     % Get list of opcodes to register for Module
-    Apps0 = maps:get(apps, St0),
-    Apps1 = reg_app(Application, Apps0),
-    {reply, ok, St0#{apps => Apps1}};
+    Apps1 = reg_app(Application, Encoder, Apps),
+    {reply, ok, St0#{apps := Apps1}};
 handle_call({decode, Message, Session}, _From, St0) ->
     Reply = route(Message, Session, St0),
     {reply, Reply, St0};
-handle_call(registered_ops, _From, St0) ->
-    Ops = maps:get(ops, St0),
-    Reply = maps:keys(Ops),
+handle_call(rpcs, _From, #{c_rpc := C, s_rpc := S} = St0) ->
+    Reply = {{client_rpc, maps:keys(C)}, {server_rpc, maps:keys(S)}},
     {reply, Reply, St0};
-handle_call(registered_apps, _From, St0) ->
+handle_call(apps, _From, St0) ->
     Apps = maps:get(apps, St0),
     {reply, Apps, St0};
-handle_call({op_info, OpCode}, _From, St0) ->
-    Ops = maps:get(ops, St0),
-    Reply = maps:get(OpCode, Ops, undefined),
+handle_call({app, App}, _From, #{apps := Apps} = St0) ->
+    Reply = proplist:lookup(App, Apps),
+    {reply, Reply, St0};
+handle_call({client_rpc, RPC}, _From, #{c_rpc := C} = St0) ->
+    Reply = maps:get(RPC, C),
+    {reply, Reply, St0};
+handle_call({server_rpc, RPC}, _From, #{s_rpc := S} = St0) ->
+    Reply = maps:get(RPC, S),
     {reply, Reply, St0}.
 
 handle_cast(_Request, St0) ->
@@ -213,51 +227,96 @@ code_change(_OldVsn, St0, _Extra) ->
 %%============================================================================
 %% Internal functions
 %%============================================================================
--spec route(<<_:16, _:_*8>>, ow_session:session(), map()) ->
+-spec route(<<_:8, _:_*8>>, ow_session:session(), map()) ->
     ow_session:net_msg().
-route(<<OpCode:16, Message/binary>>, Session, St0) ->
-    Routes = maps:get(ops, St0),
-    case maps:get(OpCode, Routes, unknown) of
-        unknown ->
-            logger:notice("Got an unknown OpCode: ~p", [OpCode]);
-        Map ->
-            case ow_rpc:c2s_handler(Map) of
-                {Module, Fun, 2} ->
-                    % Apply the callback with the rest of the message plus
-                    % current session information (authentication status, etc).
-                    %logger:debug("Calling ~p:~p(...)", [Module, Fun]),
-                    erlang:apply(Module, Fun, [Message, Session]);
-                {Module, Fun, 1} ->
-                    % The callback is called "sessionless" - e.g. heartbeats,
-                    % version request, etc.
-                    erlang:apply(Module, Fun, [Message]),
-                    % return the session
-                    {ok, Session};
-                undefined ->
-                    % There is no 'C' in this RPC.
-                    logger:notice("No RPC registered for ~p", [OpCode]),
-                    {ok, Session}
-            end
-    end.
+route(<<Prefix:8, Msg/binary>>, Session, #{apps := Apps}) ->
+    % Message will be prefixed with an application identifier
+    % Get the callback module and route the message as appropriate
+    {Mod, Fun} = orddict:fetch(Prefix, Apps),
+    erlang:apply(Mod, Fun, [Msg, Session]).
 
-reg(Module, Ops) ->
-    RPCs = erlang:apply(Module, rpc_info, []),
-    lists:foldl(fun(Item, Map) -> reg_op(Item, Map) end, Ops, RPCs).
+-spec reg_rpc(atom(), map()) -> map().
+reg_rpc(Module, #{c_rpc := CRPC, s_rpc := SRPC} = St0) ->
+    % Get module info for the module
+    AllAttributes = erlang:apply(Module, module_info, [attributes]),
+    F = fun(Attribute) ->
+        case proplists:lookup(Attribute, AllAttributes) of
+            none ->
+                #{};
+            {Attribute, Calls} ->
+                deeper_propmap(Calls)
+        end
+    end,
+    ClientMap = F(rpc_client),
+    ServerMap = F(rpc_server),
+    CRPC1 = maps:merge(ClientMap, CRPC),
+    SRPC1 = maps:merge(ServerMap, SRPC),
+    St0#{c_rpc => CRPC1, s_rpc => SRPC1}.
 
-reg_app(App, AppList) ->
-    case lists:member(App, AppList) of
-        true ->
-            AppList;
-        false ->
-            [App | AppList]
-    end.
+-spec reg_app(pos_integer(), {atom(), atom()}, atom(), list()) -> list().
+reg_app(Prefix, App, Encoder, AppList) ->
+    % Note that this will bump the next available slot up.
+    orddict:store(Prefix, {App, Encoder}, AppList).
+-spec reg_app({atom(), atom()}, atom(), list()) -> list().
+reg_app(App, Encoder, AppList) ->
+    % Determine the next available prefix
+    Next =
+        case orddict:fetch_keys(AppList) of
+            [] ->
+                0;
+            Keys ->
+                Max = lists:max(Keys),
+                Max + 1
+        end,
+    % The prefix is assumed to be 8-bits, weird stuff may happen beyond 255 apps
+    orddict:store(Next, {App, Encoder}, AppList).
 
-reg_op(Map, St0) ->
-    % Get the OpCode from the inner map
-    OpCode = maps:get(opcode, Map),
-    case maps:get(OpCode, St0, undefined) of
-        undefined ->
-            maps:put(OpCode, Map, St0);
-        _ ->
-            St0
-    end.
+-spec reg_app_test() -> ok.
+reg_app_test() ->
+    Apps0 = orddict:new(),
+    Apps1 = reg_app(foo, test_pb, Apps0),
+    ?assertEqual(true, orddict:is_key(0, Apps1)),
+    % Try adding another
+    Apps2 = reg_app(bar, test_pb, Apps1),
+    ?assertEqual(true, orddict:is_key(1, Apps2)),
+    ok.
+
+-spec reg_app_prefix_test() -> ok.
+reg_app_prefix_test() ->
+    Apps0 = orddict:new(),
+    Apps1 = reg_app(10, foo, test_pb, Apps0),
+    ?assertEqual(true, orddict:is_key(10, Apps1)),
+    % Try adding another to see if it increments properly
+    Apps2 = reg_app(bar, test_pb, Apps1),
+    ?assertEqual(true, orddict:is_key(11, Apps2)),
+    ok.
+
+-spec deeper_propmap(list()) -> map().
+deeper_propmap(PropList) ->
+    F = fun
+        ({K, V}) ->
+            proplists:to_map([{K, V}]);
+        (List) when is_list(List) ->
+            proplists:to_map(List);
+        (Atom) when is_atom(Atom) ->
+            Atom
+    end,
+    G = fun(K, V, MapIn) ->
+        Map = F(V),
+        MapIn#{K => Map}
+    end,
+    Map = proplists:to_map(PropList),
+    maps:fold(G, #{}, Map).
+
+-spec deeper_propmap_test() -> ok.
+deeper_propmap_test() ->
+    PropList = [
+        {foo, true},
+        bar,
+        {baz, {deep, true}}
+    ],
+    Map = deeper_propmap(PropList),
+    ?assertEqual(true, maps:get(foo, Map)),
+    ?assertEqual(true, maps:get(bar, Map)),
+    ?assertEqual(#{deep => true}, maps:get(baz, Map)),
+    ok.
