25131934362f6d8512224b3f4f585c1185ddf651
cmdrk <cmdrk@proton.me>
2023-05-16 00:49:17 -0500

    Add ow_msg

---
+++ src/ow_msg.erl
@@ -0,0 +1,22 @@
+-module(ow_msg).
+
+-export([decode/2, raw_decode/1, encode/2]).
+
+-spec decode(binary(), ow_session:session()) -> any().
+decode(Msg, Session) ->
+    % First decode the larger message
+    #{msg := {Call, SubMsg}} = overworld_pb:decode_msg(Msg, overworld),
+    % Lookup the appropriate server callback
+    #{module := Module} = ow_protocol:rpc(Call, server),
+    % Send the message to the submodule for further processing, and send a
+    % reply if any
+    erlang:apply(Module, Call, [SubMsg, Session]).
+
+-spec raw_decode(binary()) -> {atom(), map()}.
+raw_decode(Msg) ->
+    #{msg := {Call, SubMsg}} = overworld_pb:decode_msg(Msg, overworld),
+    {Call, SubMsg}.
+
+-spec encode(map(), atom()) -> binary().
+encode(Msg, Type) ->
+    overworld_pb:encode_msg(#{msg => {Type, Msg}}, overworld).
