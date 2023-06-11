d6b0cd9cb0f434bc838a81117e9899dc704b622e
cmdrk <cmdrk@proton.me>
2023-06-11 01:52:56 -0500

    Add generic versions of encode/decode if you follow the Overworld pattern

--- src/ow_msg.erl
+++ src/ow_msg.erl
@@ -1,15 +1,22 @@
 -module(ow_msg).
 -behaviour(ow_rpc).
 
--export([encoder/0, decode/2, raw_decode/1, encode/2]).
-
--spec encoder() -> atom().
-encoder() -> overworld_pb.
+% Special generic versions if applications implement encode/decode the same way
+% that Overworld does it.
+-export([encode/3, decode/3]).
+% Callbacks specified by the ow_rpc behaviour + debugging decoder
+-export([decode/2, raw_decode/1, encode/2]).
 
 -spec decode(binary(), ow_session:session()) -> any().
 decode(Msg, Session) ->
+    decode(Msg, Session, overworld).
+
+-spec decode(binary(), ow_session:session(), atom()) -> any().
+decode(Msg, Session, Application) ->
+    ProtoLib = add_pb_suffix(Application),
     % First decode the larger message
-    #{msg := {Call, SubMsg}} = overworld_pb:decode_msg(Msg, overworld),
+    Decoded = erlang:apply(ProtoLib, decode_msg, [Msg, Application]),
+    #{msg := {Call, SubMsg}} = Decoded,
     % Lookup the appropriate server callback
     #{module := Module} = ow_protocol:rpc(Call, server),
     % Send the message to the submodule for further processing, and send a
@@ -22,8 +29,18 @@ raw_decode(Msg) ->
     {Call, SubMsg}.
 
 -spec encode(map(), atom()) -> binary().
-encode(Msg, Type) ->
-    Prefix = ow_protocol:prefix(overworld),
+encode(SubMsg, Call) ->
+    encode(SubMsg, Call, overworld).
+
+-spec encode(map(), atom(), atom()) -> binary().
+encode(SubMsg, Call, Application) ->
+    Prefix = ow_protocol:prefix(Application),
     B1 = <<Prefix:16>>,
-    B2 = overworld_pb:encode_msg(#{msg => {Type, Msg}}, overworld),
+    ProtoLib = add_pb_suffix(Application),
+    Msg = #{msg => {Call, SubMsg}},
+    B2 = erlang:apply(ProtoLib, encode_msg, [Msg, Application]),
     <<B1/binary, B2/binary>>.
+
+-spec add_pb_suffix(atom()) -> atom().
+add_pb_suffix(Application) ->
+    list_to_existing_atom(atom_to_list(Application) ++ "_pb").
