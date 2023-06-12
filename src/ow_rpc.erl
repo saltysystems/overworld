025d0d091921847b6701c1a12de1ec2d839b4c3b
cmdrk <cmdrk@proton.me>
2023-06-11 23:06:34 -0500

    make encode/2 and decode/4 optional callbacks

--- src/ow_rpc.erl
+++ src/ow_rpc.erl
@@ -6,11 +6,23 @@
     Msg :: map(),
     Type :: atom(),
     Result :: binary().
+-callback encode(Msg, Type, Lib, App) -> Result when
+    Msg :: map(),
+    Type :: atom(),
+    Lib :: atom(),
+    App :: atom(),
+    Result :: binary().
 -callback decode(BinMsg, Session) -> Result when
     BinMsg :: binary(),
     Session :: ow_session:session(),
     Result :: any().
-
+-callback decode(BinMsg, Session, Lib, App) -> Result when
+    BinMsg :: binary(),
+    Session :: ow_session:session(),
+    Lib :: atom(),
+    App :: atom(),
+    Result :: any().
+-optional_callbacks([encode/2, decode/4]).
 -include("rpc/defaults.hrl").
 
 defaults() ->
