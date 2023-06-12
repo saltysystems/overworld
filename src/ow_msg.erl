1a2674f90969044478c8cb9ba7c7778b6ca82566
cmdrk <cmdrk@proton.me>
2023-06-11 23:01:03 -0500

    use the new encoder definition

--- src/ow_msg.erl
+++ src/ow_msg.erl
@@ -1,21 +1,27 @@
 -module(ow_msg).
 -behaviour(ow_rpc).
 
-% Special generic versions if applications implement encode/decode the same way
-% that Overworld does it.
--export([encode/3, decode/3]).
-% Callbacks specified by the ow_rpc behaviour + debugging decoder
--export([decode/2, raw_decode/1, encode/2]).
+% This module does double-duty as a generic encoder/decoder for applications
+% (encode/4, decode/4), and also an Overworld-specific application encoder
+% (encode/2, decode/2) implementing the behaviour. If application protofiles
+% are arranged in the same way as the Overworld protofiles, you can just pass
+% through to the arity 4 functions here.
+
+-export([encode/2, decode/2]).
+-export([encode/4, decode/4]).
+% Debugging decoder
+-export([raw_decode/1]).
 
 -spec decode(binary(), ow_session:session()) -> any().
 decode(Msg, Session) ->
-    decode(Msg, Session, overworld).
+    EncoderLib = overworld_pb,
+    Application = overworld,
+    decode(Msg, Session, EncoderLib, Application).
 
--spec decode(binary(), ow_session:session(), atom()) -> any().
-decode(Msg, Session, Application) ->
-    ProtoLib = add_pb_suffix(Application),
+-spec decode(binary(), ow_session:session(), atom(), atom()) -> any().
+decode(Msg, Session, EncoderLib, Application) ->
     % First decode the larger message
-    Decoded = erlang:apply(ProtoLib, decode_msg, [Msg, Application]),
+    Decoded = erlang:apply(EncoderLib, decode_msg, [Msg, Application]),
     #{msg := {Call, SubMsg}} = Decoded,
     % Lookup the appropriate server callback
     #{module := Module} = ow_protocol:rpc(Call, server),
@@ -28,19 +34,17 @@ raw_decode(Msg) ->
     #{msg := {Call, SubMsg}} = overworld_pb:decode_msg(Msg, overworld),
     {Call, SubMsg}.
 
--spec encode(map(), atom()) -> binary().
-encode(SubMsg, Call) ->
-    encode(SubMsg, Call, overworld).
-
--spec encode(map(), atom(), atom()) -> binary().
-encode(SubMsg, Call, Application) ->
+-spec encode(map(), atom(), atom(), atom()) -> binary().
+encode(SubMsg, Call, EncoderLib, Application) ->
     Prefix = ow_protocol:prefix(Application),
     B1 = <<Prefix:16>>,
-    ProtoLib = add_pb_suffix(Application),
     Msg = #{msg => {Call, SubMsg}},
-    B2 = erlang:apply(ProtoLib, encode_msg, [Msg, Application]),
+    B2 = erlang:apply(EncoderLib, encode_msg, [Msg, Application]),
     <<B1/binary, B2/binary>>.
 
--spec add_pb_suffix(atom()) -> atom().
-add_pb_suffix(Application) ->
-    list_to_existing_atom(atom_to_list(Application) ++ "_pb").
+-spec encode(map(), atom()) -> binary().
+encode(SubMsg, Call) ->
+    % If being called from Overworld, we know the EncoderLib and Application name
+    EncoderLib = overworld_pb,
+    Application = overworld,
+    encode(SubMsg, Call, EncoderLib, Application).
