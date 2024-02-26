f18a496919179539a5607dcbb266abd848f9b92e
cmdrk <cmdrk@protonmail.com>
2024-02-25 22:44:13 -0600

    The reconnecting client won't know the old ID, or shouldn't necessarily have to. Just supply the token and get an ew token and the old ID back

--- src/ow_token_serv.erl
+++ src/ow_token_serv.erl
@@ -4,7 +4,7 @@
 -export([
     start_link/0,
     new/1,
-    exchange/2
+    exchange/1
 ]).
 
 %% gen_server callbacks
@@ -27,8 +27,8 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 new(ID) ->
     gen_server:call(?MODULE, {new, ID}).
-exchange(ID, Token) ->
-    gen_server:call(?MODULE, {exchange, ID, Token}).
+exchange(Token) ->
+    gen_server:call(?MODULE, {exchange, Token}).
 
 init([]) ->
     {ok, #state{}}.
@@ -37,16 +37,16 @@ handle_call({new, ID}, _From, #state{tokens = Tokens} = State) ->
     Token = create_token(),
     Tokens1 = [{ID, Token} | Tokens],
     {reply, Token, State#state{tokens = Tokens1}};
-handle_call({exchange, ID, Token}, _From, #state{tokens = Tokens} = State) ->
-    case lists:keyfind(ID, 1, Tokens) of
+handle_call({exchange, Token}, _From, #state{tokens = Tokens} = State) ->
+    case lists:keyfind(Token, 2, Tokens) of
         false ->
             {reply, false, State};
         {ID, Token} ->
             % Token matches via pattern matching, give the user a new token
             NewToken = create_token(),
             Tokens1 = lists:keyreplace(ID, 1, Tokens, {ID, NewToken}),
-            {reply, NewToken, State#state{tokens = Tokens1}};
-        {ID, _} ->
+            {reply, {ID, NewToken}, State#state{tokens = Tokens1}};
+        {_, _} ->
             {reply, denied, State}
     end;
 handle_call(_, _, State) ->
